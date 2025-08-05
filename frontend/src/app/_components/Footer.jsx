a1a78f2f2d9b61e8cd8d47e7030ddd4082013c28
user-Gyrus <nandakumarblj@gmail.com>
2025-08-05 10:00:47 +0530

    DAY 2: Finished Footer, started with Landing Page

--- frontend/src/app/_components/Footer.jsx
+++ frontend/src/app/_components/Footer.jsx
@@ -1,5 +1,30 @@
+import { Poppins } from "next/font/google";
+import { footer } from "../_data/footerData";
+import Link from "next/link";
+
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500", "700"] });
+
 function Footer() {
-  return <div className="m-3">This is the Footer</div>;
+  return (
+    <div className="m-3 flex flex-row justify-between">
+      <div className={poppins.className}>
+        <span className="mr-2">Nanda Kumar B</span>
+        <span>2025</span>
+      </div>
+      <div className="flex flex-row gap-5">
+        {footer.map((item) => (
+          <Link
+            key={item.id}
+            href={item.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <item.icon className="text-2xl hover:opacity-75" />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
---
+++ frontend/src/app/_data/footerData.js
@@ -0,0 +1,15 @@
+import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
+
+export const footer = [
+  {
+    id: 1,
+    icon: FaInstagram,
+    url: "https://www.instagram.com/_user_nanda_04/",
+  },
+  {
+    id: 2,
+    icon: FaLinkedin,
+    url: "https://www.linkedin.com/in/nanda-kumar-balaji-483a38255/",
+  },
+  { id: 3, icon: FaGithub, url: "https://github.com/user-Gyrus" },
+];
